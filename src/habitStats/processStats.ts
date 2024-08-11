a9f8358d31d307bab964bbdbe6efc14ebc9e718b
timabloom <timabloom@proton.me>
2024-08-11 13:44:07 +0200

    refactor: broke out processingStats to its own file

---
+++ src/habitStats/processStats.ts
@@ -0,0 +1,45 @@
+import { HabitsData } from "../db";
+
+type ActivityStats = { id: string; date: string;[x: string]: string | number; }[]
+
+export function getAllActivityStats(habitsData: HabitsData) {
+    const allActivityStats: ActivityStats = [];
+
+    habitsData.oldHabits.map(habit => {
+        habit.timeSpent.map(timeSpent => {
+            const activityExist = allActivityStats.find(d => d.date === timeSpent.date.slice(5));
+            if (!activityExist) {
+                allActivityStats.push({ id: timeSpent.id, date: timeSpent.date.slice(5), [habit.name]: timeSpent.minutes });
+            } else {
+                activityExist[habit.name] = timeSpent.minutes;
+            }
+        });
+    });
+
+    habitsData.newHabits.map(habit => {
+        habit.timeSpent.map(timeSpent => {
+            const activityExist = allActivityStats.find(d => d.date === timeSpent.date.slice(5));
+            if (!activityExist) {
+                allActivityStats.push({ id: timeSpent.id, date: timeSpent.date.slice(5), [habit.name]: timeSpent.minutes });
+            } else {
+                activityExist[habit.name] = timeSpent.minutes;
+            }
+        });
+    });
+    return allActivityStats;
+}
+
+export function getPreviousSevenDays(stats: ActivityStats) {
+    const previousSevenDays: ActivityStats = [];
+    const currentDay = parseInt(new Date().toJSON().slice(8, 10));
+    const currentMonth = new Date().toJSON().slice(5, 7);
+    for (let i = currentDay - 1; i > currentDay - 8; i--) {
+        const activityDay = stats.find(activity => parseInt(activity.date.slice(3)) === i);
+        if (activityDay) {
+            previousSevenDays.unshift(activityDay);
+        } else {
+            previousSevenDays.unshift({ id: `${i}`, date: `${i > 9 ? `${currentMonth}-${i}` : `${currentMonth}-0${i}`}` });
+        }
+    }
+    return previousSevenDays;
+}
--- src/habitStats/weekBarChart.tsx
+++ src/habitStats/weekBarChart.tsx
@@ -1,70 +1,22 @@
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from "recharts";
 import { HabitsData } from "../db";
 import uniqolor from "uniqolor";
-
-type HabitsStats = { id: string; name: string }[]
-type ActivityStats = { id: string; date: string;[x: string]: string | number; }[]
+import { getAllActivityStats, getPreviousSevenDays } from "./processStats";
 
 function WeekBarChart(props: { habitsData: HabitsData }) {
-
-    const oldHabitsStats: HabitsStats = [];
-    const newHabitsStats: HabitsStats = [];
-    const allActivityStats: ActivityStats = [];
-
-    props.habitsData.oldHabits.map(habit => {
-        habit.timeSpent.map(timeSpent => {
-            const habitExist = oldHabitsStats.find(name => name.name === habit.name);
-            if (!habitExist) {
-                oldHabitsStats.push({ id: habit.id, name: habit.name });
-            }
-            const activityExist = allActivityStats.find(d => d.date === timeSpent.date.slice(5));
-            if (!activityExist) {
-                allActivityStats.push({ id: timeSpent.id, date: timeSpent.date.slice(5), [habit.name]: timeSpent.minutes });
-            } else {
-                activityExist[habit.name] = timeSpent.minutes;
-            }
-        });
-    });
-
-    props.habitsData.newHabits.map(habit => {
-        habit.timeSpent.map(timeSpent => {
-            const habitExist = newHabitsStats.find(name => name.name === habit.name);
-            if (!habitExist) {
-                newHabitsStats.push({ id: habit.id, name: habit.name });
-            }
-            const activityExist = allActivityStats.find(d => d.date === timeSpent.date.slice(5));
-            if (!activityExist) {
-                allActivityStats.push({ id: timeSpent.id, date: timeSpent.date.slice(5), [habit.name]: timeSpent.minutes });
-            } else {
-                activityExist[habit.name] = timeSpent.minutes;
-            }
-        });
-    });
-
-    const currentDay = parseInt(new Date().toJSON().slice(8, 10));
-    const currentMonth = new Date().toJSON().slice(5, 7);
-    const previousSevenDays: ActivityStats = [];
-    for (let i = currentDay - 1; i > currentDay - 8; i--) {
-        const activityDay = allActivityStats.find(activity => parseInt(activity.date.slice(3)) === i);
-        if (activityDay) {
-            previousSevenDays.unshift(activityDay);
-        } else {
-            previousSevenDays.unshift({ id: `${i}`, date: `${i > 9 ? `${currentMonth}-${i}` : `${currentMonth}-0${i}`}` });
-        }
-    }
 
     return (
         <div className="w-auto mb-20 mr-5" >
             <ResponsiveContainer height={240}>
-                <BarChart data={previousSevenDays} >
+                <BarChart data={getPreviousSevenDays(getAllActivityStats(props.habitsData))} >
                     <XAxis dataKey="date" stroke="#000" />
                     <YAxis />
                     <Tooltip />
                     <Legend />
-                    {oldHabitsStats.map(habit =>
+                    {props.habitsData.oldHabits.map(habit =>
                         <Bar key={habit.id} dataKey={habit.name} fill={`${uniqolor.random().color}`} barSize={10} />
                     )}
-                    {newHabitsStats.map(habit =>
+                    {props.habitsData.newHabits.map(habit =>
                         <Bar key={habit.id} dataKey={habit.name} stackId="a" fill={`${uniqolor.random().color}`} barSize={10} />
                     )}
                 </BarChart>
