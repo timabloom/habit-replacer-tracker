100719f01e162aad0cf8750e8e77e1965b5748aa
timabloom <timabloom@proton.me>
2024-08-12 09:25:03 +0200

    feat: add style for statistics component

--- src/habitStats/habitStats.tsx
+++ src/habitStats/habitStats.tsx
@@ -3,7 +3,13 @@ import WeekBarChart from "./weekBarChart";
 
 function HabitsStats(props: { habitsData: HabitsData }) {
     return (
-        <WeekBarChart habitsData={props.habitsData} />
+        <>
+            <div className="flex flex-col items-center">
+                <h1 className="text-3xl p-10 pt-16 pb-8">Statistics</h1>
+                <h2 className="text-2xl p-5 pb-8">Activity Stats: Last 7 Days</h2>
+            </div>
+            <WeekBarChart habitsData={props.habitsData} />
+        </>
     );
 }
 
