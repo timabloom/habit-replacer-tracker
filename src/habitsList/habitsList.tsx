3edeb5046c952fd547616ca2af4a128cf57aaef6
timabloom <timabloom@proton.me>
2024-08-13 07:22:50 +0200

    feat: added habits follow flex column

--- src/habitsList/habitsList.tsx
+++ src/habitsList/habitsList.tsx
@@ -9,7 +9,7 @@ function HabitsList(props: { habitsData: HabitsData, habitState: HabitStateManag
         <div className="flex flex-col items-center">
             <h1 className="text-3xl p-10 pt-16 pb-8">Your Habits</h1>
             <h2 className="text-2xl p-5 pb-8">Old Habits to be Replaced</h2>
-            <div className="flex flex-wrap justify-center max-w-64">
+            <div className="flex flex-col max-w-64">
                 {props.habitsData.oldHabits.map(habit =>
                     <div key={habit.id}>
                         <h3 className="text-xl">{habit.name}</h3>
@@ -19,7 +19,7 @@ function HabitsList(props: { habitsData: HabitsData, habitState: HabitStateManag
                 <AddHabitModal habitType="old" habitsData={props.habitsData} habitState={props.habitState} />
             </div>
             <h2 className="text-2xl p-5 pb-8 pt-8">New Habits to Cultivate</h2>
-            <div className="flex flex-wrap justify-center max-w-64">
+            <div className="flex flex-col max-w-64">
                 {props.habitsData.newHabits.map(habit =>
                     <div key={habit.id}>
                         <h3 className="text-xl">{habit.name}</h3>
